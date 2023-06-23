696b84540d61b6503ee54dc809ed300b50f072ad
Herbert <shdelgado.m@gmail.com>
2023-06-23 13:48:46 -0400

    se quito  https://magnificent-gaufre-a5c092.netlify.app/Inicio

--- django_crud_api/settings.py
+++ django_crud_api/settings.py
@@ -141,7 +141,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
      "http://localhost:3000",
      "https://ratingiea.netlify.app",
-     "https://magnificent-gaufre-a5c092.netlify.app",
     # "https://example.com",
     
 ]
